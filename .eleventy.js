cc8bf2eb37e9f94294ee5910f33902b2b7d4e75c
Chris Enns <chris.enns@gmail.com>
2022-01-28 23:43:20 -0600

    I don't fn know.

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,35 @@ const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 
-const TransistorFmPlugin = require("@11ty/eleventy-data-transistor-fm");
+const TransistorFm = require("@11ty/eleventy-data-transistor-fm");
+const pkg = require("./package.json");
+
+require('dotenv').config();
+
+module.exports = function(eleventyConfig, options = {}) {
+  try {
+    eleventyConfig.versionCheck(pkg["11ty"].compatibility);
+  } catch(e) {
+    console.log( `WARN: Eleventy Plugin (${pkg.name}) Compatibility: ${e.message}` );
+  }
+
+  let opts = Object.assign({
+    envApiKey: "TRANSISTOR_FM_API_KEY",
+    envApiKeyRequired: true,
+    data: "transistorfm.episodes",
+  }, options);
+
+  let apiKey = process.env[opts.envApiKey];
+  if(opts.envApiKeyRequired && !apiKey) {
+    throw new Error(`The environment variable ${opts.envApiKey} is missing. Do you have an .env file?`)
+  }
+
+  eleventyConfig.addGlobalData(opts.data, async function() {
+    return TransistorFm({
+      apiKey
+    });
+  });
+};
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -18,8 +46,6 @@ module.exports = function (eleventyConfig) {
       "dd LLL yyyy"
     );
   });
-
-  
 
   // Syntax Highlighting for Code blocks
   eleventyConfig.addPlugin(syntaxHighlight);
